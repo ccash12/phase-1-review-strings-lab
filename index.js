d636e608fcb4fd184f983181b38a443f6cdd4c38
Courtney Cash <cashcourtney97@gmail.com>
2021-08-21 11:37:31 -0400

    commit tree c9de817ce14414a00ca906b35fa22fc91fa9ca6c

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-// Write your code in this file!
-const currentUser = 'Grace Hopper';
+const currentUser = "Grace Hopper";
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//interpolation ^
 
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+//const welcomeMessage = 'Welcome to Flatbook,' + currentUser;
+// ^ concatenate
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+// in the variable excitedWelcomeMessage, it is referencing the variable welcomeMessage and making it uppercase using .toUpperCase()
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
